1c9a699c792d81ee60b0fe7883f5feed93282431
Avram Korets <abrasha@gmail.com>
2022-08-19 14:37:01 +0300

    refactor by pylint guide

---
+++ __init__.py
@@ -0,0 +1,2 @@
+""" this is init file """
+__all__ =  ['create_excel', 'test_create_excel']
--- test_create_excel.py
+++ test_create_excel.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
+"""Module that testing creating of excel with default parameters."""
 from loguru import logger
-from create_excel import *
+from create_excel import create_excel, Maslul, MortgageTypes
 
 
 @logger.catch
 def main():
+    """function that running all tests"""
     create_excel('calc.xlsx',[Maslul(MortgageTypes.Kalaz,                   4,   20*12, 450000),
                               Maslul(MortgageTypes.Praim,                   1,   30*12, 250000),
                               Maslul(MortgageTypes.FiveYearsPercent,      3.5,   20*12, 100000),
                               Maslul(MortgageTypes.FiveYearsPercentZamud,   3,   15*12, 100000),
                               ])
-    pass
-    
+
 if __name__ == "__main__":
     main()
